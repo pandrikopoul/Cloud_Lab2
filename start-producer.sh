c809f72ac28344918847f92825c5a5b248c8bccb
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-21 21:49:39 +0300

    Add files via upload

---
+++ start-producer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USAGE="
+Usage: start-producer.sh <topic>
+
+    topic: The topic where messages are to be produced.
+"
+
+if ! (( $# > 0 )); then
+    echo "$USAGE"
+    exit 1
+fi
+
+topic="$1"
+
+for i in {1..3}; do
+    docker run \
+        --rm \
+        -v $(pwd)/auth:/usr/src/cc-assignment-2023/experiment-producer/auth \
+        image/experiment-producer \
+        --topic "$topic" &
+done
