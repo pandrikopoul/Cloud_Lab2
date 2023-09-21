7dc8883e07bf64ec33efb1cb15d87030465b7aab
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-21 21:41:43 +0300

    Add files via upload

---
+++ Lab_2_consumer.py
@@ -0,0 +1,47 @@
+import click
+from confluent_kafka import Consumer, KafkaError
+from avro.io import DatumReader, BinaryDecoder
+
+c = Consumer({
+    'bootstrap.servers': '13.49.128.80:19093',
+    'group.id': 'simple_consumer',
+    'auto.offset.reset': 'latest',
+    'security.protocol': 'SSL',
+    'ssl.ca.location': './auth/ca.crt',
+    'ssl.keystore.location': './auth/kafka.keystore.pkcs12',
+    'ssl.keystore.password': 'cc2023',
+    'enable.auto.commit': 'true',
+    'ssl.endpoint.identification.algorithm': 'none',
+})
+
+
+@click.command()
+@click.argument('topic')
+def consume(topic: str):
+    c.subscribe([topic])
+
+    while True:
+        msg = c.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                print('Reached end of partition')
+            else:
+                print('Error while polling message:', msg.error())
+            continue
+
+
+        decoder = BinaryDecoder(msg.value())
+        reader = DatumReader()
+        decoded_message = reader.read(decoder)
+
+
+        record_name = decoded_message['record_name']
+        data = decoded_message['deserialized_message']
+
+        print(record_name)
+        print(data)
+
+
+consume()
