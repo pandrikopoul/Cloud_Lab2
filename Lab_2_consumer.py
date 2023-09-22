e3d1ea05dec7b58449f6c9f26b43c2603b4e89ea
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-22 07:11:00 +0300

    Lab_2_consumer.py

--- Lab_2_consumer.py
+++ Lab_2_consumer.py
@@ -2,24 +2,18 @@ import click
 import random
 import signal
 import io
-import json
 import fastavro
 from fastavro.schema import parse_schema
 from fastavro.schema import load_schema
 from fastavro.schema import extract_record_type
-
-from avro.io import BinaryDecoder, DatumReader
 from confluent_kafka import Consumer, KafkaError
 
-# Συνάρτηση για τη διαχείριση σήματος τερματισμού
 def signal_handler(sig, frame):
     print('EXITING SAFELY!')
     exit(0)
 
-# Ορισμός σήματος τερματισμού
 signal.signal(signal.SIGTERM, signal_handler)
 
-# Δημιουργία καταναλωτή Kafka
 c = Consumer({
     'bootstrap.servers': '13.49.128.80:19093,13.49.128.80:29093,13.49.128.80:39093',
     'group.id': f"{random.random()}",
@@ -32,7 +26,6 @@ c = Consumer({
     'ssl.endpoint.identification.algorithm': 'none',
 })
 
-# Εντολή για τη διαχείριση του CLI με το Click
 @click.command()
 @click.argument('topic')
 def consume(topic: str):
@@ -49,19 +42,9 @@ def consume(topic: str):
         avro_message = msg.value()
 
         try:
-            avro_message_dict = json.loads(avro_message)
-            avro_schema = avro_message_dict.get('schema')
-            avro_payload = avro_message_dict.get('payload')
-
-            if avro_schema and avro_payload:
-                schema = parse_schema(avro_schema)
-                record_type = extract_record_type(schema)
-                reader = fastavro.schemaless_reader(io.BytesIO(avro_payload), schema, record_type=record_type)
-                print(reader)
-            else:
-                print("Invalid Avro message format.")
+            reader = fastavro.schemaless_reader(io.BytesIO(avro_message))
+            print(reader)
         except Exception as e:
             print(f"Error decoding Avro message: {e}")
 
-# Εκτέλεση της εφαρμογής καταναλωτή
 consume()
