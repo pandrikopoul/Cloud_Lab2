062b99038300539acf1616916536e28d14490594
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-22 07:04:32 +0300

    Lab_2_consumer.py

--- Lab_2_consumer.py
+++ Lab_2_consumer.py
@@ -1,18 +1,25 @@
 import click
-from confluent_kafka import Consumer, KafkaError
-import io
 import random
 import signal
-from avro.io import BinaryDecoder, DatumReader
-import fastavro
+import io
 import json
+import fastavro
+from fastavro.schema import parse_schema
+from fastavro.schema import load_schema
+from fastavro.schema import extract_record_type
 
+from avro.io import BinaryDecoder, DatumReader
+from confluent_kafka import Consumer, KafkaError
+
+# Συνάρτηση για τη διαχείριση σήματος τερματισμού
 def signal_handler(sig, frame):
     print('EXITING SAFELY!')
     exit(0)
 
+# Ορισμός σήματος τερματισμού
 signal.signal(signal.SIGTERM, signal_handler)
 
+# Δημιουργία καταναλωτή Kafka
 c = Consumer({
     'bootstrap.servers': '13.49.128.80:19093,13.49.128.80:29093,13.49.128.80:39093',
     'group.id': f"{random.random()}",
@@ -25,6 +32,7 @@ c = Consumer({
     'ssl.endpoint.identification.algorithm': 'none',
 })
 
+# Εντολή για τη διαχείριση του CLI με το Click
 @click.command()
 @click.argument('topic')
 def consume(topic: str):
@@ -39,17 +47,21 @@ def consume(topic: str):
             continue
 
         avro_message = msg.value()
+
         try:
             avro_message_dict = json.loads(avro_message)
             avro_schema = avro_message_dict.get('schema')
             avro_payload = avro_message_dict.get('payload')
-            
+
             if avro_schema and avro_payload:
-                reader = fastavro.schemaless_reader(io.BytesIO(avro_payload), avro_schema)
+                schema = parse_schema(avro_schema)
+                record_type = extract_record_type(schema)
+                reader = fastavro.schemaless_reader(io.BytesIO(avro_payload), schema, record_type=record_type)
                 print(reader)
             else:
                 print("Invalid Avro message format.")
         except Exception as e:
             print(f"Error decoding Avro message: {e}")
 
+# Εκτέλεση της εφαρμογής καταναλωτή
 consume()
