075c640b1ee13d0333ba5857382f3384ce649215
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-09-22 03:43:03 +0300

    Lab_2_consumer.py

--- Lab_2_consumer.py
+++ Lab_2_consumer.py
@@ -1,8 +1,7 @@
 import click
-from confluent_kafka import Consumer, KafkaError
-from avro.io import DatumReader, BinaryDecoder
 import random
 import signal
+import fastavro
 
 def signal_handler(sig, frame):
     print('EXITING SAFELY!')
@@ -10,21 +9,23 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGTERM, signal_handler)
 
-c = Consumer({
-    'bootstrap.servers': '13.49.128.80:19093,13.49.128.80:29093,13.49.128.80:39093',
-    'group.id': f"{random.random()}",
-    'auto.offset.reset': 'latest',
-    'security.protocol': 'SSL',
-    'ssl.ca.location': './auth/ca.crt',
-    'ssl.keystore.location': './auth/kafka.keystore.pkcs12',
-    'ssl.keystore.password': 'cc2023',
-    'enable.auto.commit': 'true',
-    'ssl.endpoint.identification.algorithm': 'none',
-})
-
 @click.command()
 @click.argument('topic')
 def consume(topic: str):
+    from confluent_kafka import Consumer, KafkaError
+
+    c = Consumer({
+        'bootstrap.servers': '13.49.128.80:19093,13.49.128.80:29093,13.49.128.80:39093',
+        'group.id': f"{random.random()}",
+        'auto.offset.reset': 'latest',
+        'security.protocol': 'SSL',
+        'ssl.ca.location': './auth/ca.crt',
+        'ssl.keystore.location': './auth/kafka.keystore.pkcs12',
+        'ssl.keystore.password': 'cc2023',
+        'enable.auto.commit': 'true',
+        'ssl.endpoint.identification.algorithm': 'none',
+    })
+
     c.subscribe([topic], on_assign=lambda _, p_list: print(p_list))
 
     while True:
@@ -35,16 +36,21 @@ def consume(topic: str):
             print("Consumer error: {}".format(msg.error()))
             continue
 
-        decoder = BinaryDecoder(msg.value())
-        reader = DatumReader()
-        decoded_message = reader.read(decoder)
-
-        if decoded_message is not None:
-            record_name = decoded_message['name']
-            data = decoded_message['fields']
+        avro_message = msg.value()
+        
+        # Ανάγνωση του μηνύματος Avro με αυτόματη αναγνώριση του σχήματος
+        try:
+            decoded_message = fastavro.schemaless_reader(avro_message)
+            record_name = decoded_message['record_name']
+            data = decoded_message['deserialized_message']
             print(record_name)
             print(data)
-        else:
-            print("Received NoneType message")
+        except fastavro.read.FastavroError as e:
+            print(f"Error reading Avro message: {e}")
+            continue
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            continue
 
-consume()
+if __name__ == '__main__':
+    consume()
