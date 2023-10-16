512447ebd9004a619b0faf8e875fe385ee1b54ea
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-10-16 13:50:13 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -204,7 +204,7 @@ def consume(topic: str):
             #print(decoded_message)
        # except Exception as e:
        #     print(f"Error decoding Avro message: {e}")
-        if msg.headers[0][1]==b'experiment_terminated':
+        if msg.headers()[0][1]== b'experiment_terminated':
             print("----------------------------------EXPERIMENT - TERMINATED --------------------------------------------------")
             del experiment_dict[experiment_k]
     experiment_k=experiment_k+1
