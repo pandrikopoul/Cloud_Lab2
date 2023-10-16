adcdc7e6155f2bcf84d304088c9f9738c8c62377
pandrikopoul <124565200+pandrikopoul@users.noreply.github.com>
2023-10-16 18:31:49 +0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -76,7 +76,7 @@ def consume(topic: str):
         #try:
 
         reader = fastavro.reader(io.BytesIO(avro_message))
-        print(reader)
+        print(list(reader))
         for decoded_message in reader:
             #print("Another message ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
             
